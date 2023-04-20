dbe7fd188ef77083e5244a874b19d775317639b6
AshokButra <118210836+AshokButra@users.noreply.github.com>
2023-04-20 12:28:59 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -31,64 +31,40 @@ const Header = props => {
             </Link>
           </li>
           <li>
-            <Link to="/products" className="nav-link">
+            <Link to="/my-profile" className="nav-link">
               <FaUserAlt className="profile-icon" />
               MY PROFILE
             </Link>
           </li>
           <li>
-            <Link to="/cart" className="nav-link">
+            <Link to="/my-quote" className="nav-link">
               <AiOutlineAudit className="quote-icon" />
               GET QUOTE
             </Link>
           </li>
         </ul>
-        <button
-          type="button"
-          className="logout-desktop-btn"
-          onClick={onClickLogout}
-        >
+        <button type="button" className="logout-desktop-btn">
           Logout
         </button>
-        <button
-          type="button"
-          className="logout-mobile-btn"
-          onClick={onClickLogout}
-        >
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
-            alt="logout icon"
-            className="logout-icon"
-          />
+        <button type="button" className="logout-mobile-btn">
+          Logout
         </button>
       </div>
       <div className="nav-menu-mobile">
         <ul className="nav-menu-list-mobile">
-          <Link to="/">
+          <Link to="/my-moves">
             <li className="nav-menu-item-mobile">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
-                alt="nav home"
-                className="nav-bar-image"
-              />
+              <FaTruck className="truck-icon" />
             </li>
           </Link>
-          <Link to="/products">
+          <Link to="/my-profile">
             <li className="nav-menu-item-mobile">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
-                alt="nav products"
-                className="nav-bar-image"
-              />
+              <FaUserAlt className="profile-icon" />
             </li>
           </Link>
-          <Link to="/cart">
+          <Link to="/my-quote">
             <li className="nav-menu-item-mobile">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-                alt="nav cart"
-                className="nav-bar-image"
-              />
+              <AiOutlineAudit className="quote-icon" />
             </li>
           </Link>
         </ul>
@@ -97,3 +73,4 @@ const Header = props => {
   )
 }
 export default withRouter(Header)
+
