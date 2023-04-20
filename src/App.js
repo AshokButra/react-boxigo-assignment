50c7b2a44a135da488bdf584080f4ba8019b14dc
AshokButra <118210836+AshokButra@users.noreply.github.com>
2023-04-20 12:36:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
-import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import MyMoves from './components/MyMoves'
 
@@ -9,7 +8,6 @@ import './App.css'
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/login" component={LoginForm} />
       <Route exact path="/" component={Home} />
       <Route exact path="/my-moves" component={MyMoves} />
     </Switch>
